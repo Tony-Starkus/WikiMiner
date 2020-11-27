584ff56c416fd5ff6a3f8b695b5cec7dc2004b6f
Starkus1999 <thalisson.bandeira@hotmail.com>
2020-11-27 16:06:41 -0500

    Update MATLAB code credits

--- utils/matlab-codes-revised/runWikiFactorModel.m
+++ utils/matlab-codes-revised/runWikiFactorModel.m
@@ -1,4 +1,6 @@
-% Codes by: Raoni Ferreira.
+% Este código foi fornecido por Raoni S. Ferreira e implementa o método publicado no paper:
+% Ferreira, R.S., Pimentel, M.d.G. and Cristo, M. (2018), A wikification prediction model based on the combination of latent, dyadic, and monadic features. Journal of
+% the Association for Information Science and Technology, 69: 380-394. https://doi.org/10.1002/asi.23922
 
 clear all;
 
